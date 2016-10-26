f4752ceec23a3b04fd2058c73cfcb8da7e89fd27
JFreyra <efreyra@stuy.edu>
2016-10-26 15:22:47 -0400

    added initial randFile.c

---
+++ randFile.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+
+int randomNumber(){
+  
+  int randomData = open("/dev/random",O_RDONLY);
+  int randomNumber;
+  int size = read(randomData,randomNumber,4);
+  printf("randomNumber: %d\n",randomNumber);
+  printf("size == 4? size: %d\n",size);
+  close(randomData);
+
+  return 0;
+}
+
+void main(){
+
+  randomNumber();
+
+}
